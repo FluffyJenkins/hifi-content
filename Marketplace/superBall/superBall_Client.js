721cab476f57f89aa154fd54eb5faa5a1cc7eac8
Fluffy Jenkins <richardjrn.w@gmail.com>
2018-02-07 00:01:54 +0000

    Major changes

---
+++ Marketplace/superBall/superBall_Client.js
@@ -0,0 +1,69 @@
+// copyright 2018
+// Fluffy Jenkins
+
+var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+var isActive = false;
+
+var appIconActive = Script.resolvePath("iconBlack.svg");
+var appIconInactive = Script.resolvePath("iconWhite.svg");
+
+var _entityID;
+
+var MODEL_URL = Script.resolvePath("Ball.fbx");
+var GRAVITY = {x: 0, y: -7, z: 0};
+var BALL_SIZE = {x: 0.5, y: 0.5, z: 0.5};
+var DENSITY = 100;
+
+var SCRIPT_URL = Script.resolvePath("superBall_Entity.js?");
+
+var positionOffset = -0.5;
+
+function activateFunction() {
+
+    var position = Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(Quat.cancelOutRollAndPitch(Camera.orientation), {z: positionOffset}));
+    _entityID = Entities.addEntity({
+        type: "Model",
+        modelURL: MODEL_URL,
+        name: "SuperBall",
+        dimensions: BALL_SIZE,
+        density: DENSITY,
+        dynamic: true,
+        position: position,
+        script: SCRIPT_URL,
+        collisionless: true,
+        shapeType: "simple-hull",
+        restitution: 1,
+        gravity: {x: 0, y: 0, z: 0}
+    });
+}
+
+function deactivateFunction() {
+    Entities.deleteEntity(_entityID);
+}
+
+
+var toggle = function () {
+    isActive = !isActive;
+    activeButton.editProperties({isActive: isActive});
+    if (isActive) {
+        activateFunction();
+    } else {
+        deactivateFunction();
+    }
+};
+
+var activeButton = tablet.addButton({
+    icon: appIconInactive,
+    activeIcon: appIconActive,
+    text: "SUPERBALL",
+    isActive: false,
+    sortOrder: 10
+});
+
+Script.scriptEnding.connect(function () {
+    activeButton.clicked.disconnect(toggle);
+    tablet.removeButton(activeButton);
+    deactivateFunction();
+});
+
+activeButton.clicked.connect(toggle);
--- Marketplace/superBall/superBall_Entity.js
+++ Marketplace/superBall/superBall_Entity.js
@@ -1,153 +1,80 @@
+// copyright 2018
+// Fluffy Jenkins
+
 (function () {
 
-    var debug = false;
-
-    function debugPrint(msg){
-        if(debug){
-            print(msg);
-        }
-    }
-
-    var collisionTimeout = false;
     var _entityID;
-    var SCRIPT_INTERVAL;
+    var VELOCITY_MULTIPLIER = 1.4;
+    var VELOCITY_LIMITER_MAX = 11;
+    var VELOCITY_LIMITER_MIN = 2;
+    var BOUNCE_DELAY = 10;
+    var SOUND_URL = Script.resolvePath("Boing.wav");
+    var SOUND_VOLUME = 0.03;
+    var soundURL = SoundCache.getSound(SOUND_URL);
     var GRAVITY = {x: 0, y: -7, z: 0};
+    var injector = null;
+    var timeout = 10 * 1000;
+    var _timeout = "";
+    var positionOffset = -0.75;
+
+    this.preload = function (entityID) {
+        _entityID = entityID;
+        Entities.editEntity(_entityID, {
+            gravity: {x: 0, y: 0, z: 0}
+        });
+    };
 
     this.collisionWithEntity = function (myID, otherID, collisionInfo) {
-        debugPrint("PINGFSADFSFDSFASD");
-        debugPrint(JSON.stringify(collisionInfo));
         bounce(collisionInfo);
     };
 
-    function addParts(vec) {
-        var v;
-        v = Math.abs(vec.x) + Math.abs(vec.y) + Math.abs(vec.z);
-        return v;
-    }
-
-    var lastBouncePosition = {x:0,y:0,z:0};
-
-    function bounce(collisionInfo) {
-        debugPrint("Pass0");
-        if ((addParts(collisionInfo.velocityChange)) > 1) {
-            var entProps = Entities.getEntityProperties(_entityID, ["position", "velocity"]);
-            debugPrint("dist "+JSON.stringify(Vec3.distance(lastBouncePosition, entProps.position)));
-            if (!collisionTimeout && Vec3.distance(lastBouncePosition, entProps.position) > 0.1) {
-                var vel = {x: 0, y: 0, z: 0};
-                debugPrint("Pass1");
-
-                entProps.velocity = Vec3.sum(entProps.velocity, collisionInfo.velocityChange);
-                //debugPrint((Math.abs(entProps.velocity.x) + Math.abs(entProps.velocity.y) + Math.abs(entProps.velocity.z)));
-                if ((Math.abs(entProps.velocity.x) + Math.abs(entProps.velocity.y) + Math.abs(entProps.velocity.z)) > 0.1) {
-                    debugPrint("Pass2");
-                    var dir = Vec3.normalize(Vec3.subtract(collisionInfo.contactPoint, entProps.position));
-                    debugPrint("dir " + JSON.stringify(dir));
-                    if (Vec3.length(dir) < 0.1) {
-                        dir = {x: 0, y: -1, z: 0};
-                    }
-                    var pickRay = {
-                        origin: entProps.position,
-                        direction: dir
-                    };
-                    var intersection = Entities.findRayIntersection(pickRay, true, [], [_entityID]);
-                    if (intersection.intersects) {
-                        debugPrint("Pass3");
-                        debugPrint("Distance " + intersection.distance);
-                        debugPrint("Intersection! " + JSON.stringify(intersection));
-                        debugPrint("Ping! " + JSON.stringify(entProps.velocity));
-                        var reflect = Vec3.reflect(pickRay.direction, intersection.surfaceNormal);
-                        vel = Vec3.multiply(-1.1, Vec3.multiplyVbyV(reflect, entProps.velocity));
-                        debugPrint("Ping2! " + JSON.stringify(vel));
-
-                        Entities.editEntity(_entityID, {velocity: vel});
-
-                        lastBouncePosition = entProps.position;
-
-                        //rezSlaveAndSwitch(vel);
-                        /*
-                        var allProps = Entities.getEntityProperties(_entityID);
-                        allProps.velocity = vel;
-                        Entities.addEntity(allProps, true);
-                        Entities.deleteEntity(_entityID);*/
-                        collisionTimeout = true;
-                        Script.setTimeout(function () {
-                            collisionTimeout = false;
-                            //Entities.editEntity(_entityID, {gravity: GRAVITY});
-                        }, 300);
-                    }
-                }
-            }
-
+    this.releaseGrab = function (entityID, args) {
+        Entities.editEntity(_entityID, {gravity: GRAVITY, collisionless: false});
+        if (_timeout != "") {
+            Script.clearTimeout(_timeout);
         }
-
-        function update(delta) {
-            if (!collisionTimeout) {
-                var vel = {x: 0, y: 0, z: 0};
-
-                var entProps = Entities.getEntityProperties(_entityID, ["position", "velocity"]);
-                //debugPrint((Math.abs(entProps.velocity.x) + Math.abs(entProps.velocity.y) + Math.abs(entProps.velocity.z)));
-                if ((Math.abs(entProps.velocity.x) + Math.abs(entProps.velocity.y) + Math.abs(entProps.velocity.z)) > 0.1) {
-                    var dir = Vec3.normalize(entProps.velocity);
-                    if (Vec3.length(dir) < 0.1) {
-                        dir = {x: 0, y: -1, z: 0};
-                    }
-                    var pickRay = {
-                        origin: entProps.position,
-                        direction:
-                        dir
-                    };
-                    var intersection = Entities.findRayIntersection(pickRay, true, [], [_entityID]);
-                    if (intersection.intersects && intersection.distance < 0.5) {
-                        debugPrint("Distance " + intersection.distance);
-                        debugPrint("Intersection! " + JSON.stringify(intersection));
-                        debugPrint("Ping! " + JSON.stringify(entProps.velocity));
-                        var reflect = Vec3.reflect(pickRay.direction, intersection.surfaceNormal);
-                        vel = Vec3.multiply(1.1, Vec3.multiplyVbyV(reflect, entProps.velocity));
-                        debugPrint("Ping2! " + JSON.stringify(vel));
-
-                        Entities.editEntity(_entityID, {velocity: vel});
-
-                        //rezSlaveAndSwitch(vel);
-                        /*
-                        var allProps = Entities.getEntityProperties(_entityID);
-                        allProps.velocity = vel;
-                        Entities.addEntity(allProps, true);
-                        Entities.deleteEntity(_entityID);*/
-                        collisionTimeout = true;
-                        Script.setTimeout(function () {
-                            collisionTimeout = false;
-                            //Entities.editEntity(_entityID, {gravity: GRAVITY});
-                        }, 100);
-                    }
-                }
-            }
-        }
-    }
-
-    this.unload = function (entityID) {
-        Script.clearInterval(SCRIPT_INTERVAL);
+        _timeout = Script.setTimeout(returnToAvatar, timeout);
     };
 
-    this.preload = function (entityID) {
-        this.entityID = entityID;
-        _entityID = entityID;
-        Entities.editEntity(this.entityID, {
-            name: "FloofTest",
-            dimensions: {x: 0.5, y: 0.5, z: 0.5},
-            density: 100,
-            dynamic: true,
-            collisionless: false,
-            restitution: 1000,
-            gravity: GRAVITY,
-            lifetime: 360
-        });
-        //SCRIPT_INTERVAL = Script.setInterval(update, 10);
-        //rezSlaveAndSwitch({});
+
+    function playSound() {
+        if (!injector) {
+            var entProps = Entities.getEntityProperties(_entityID, ["position"]);
+            injector = Audio.playSound(soundURL, {position: entProps.position, volume: SOUND_VOLUME});
+        }
+        else {
+            injector.stop();
+            injector.restart();
+        }
     }
 
+    function returnToAvatar() {
+        _timeout = "";
+        var position = Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(Quat.cancelOutRollAndPitch(Camera.orientation), {z: positionOffset}));
+        Entities.editEntity(_entityID, {
+            gravity: {x: 0, y: 0, z: 0},
+            velocity: {x: 0, y: 0, z: 0},
+            dynamic: false,
+            collisionless: true,
+            position: position
+        });
+        Script.setTimeout(function () {
+            Entities.editEntity(_entityID, {
+                dynamic: true,
+                velocity: {x: 0, y: 0, z: 0}
+            });
+        }, BOUNCE_DELAY);
+    }
 
-})
-
-//file:///D:\High Fidelity\superBall_Entity.js
-
-
+    function bounce(collisionInfo) {
+        if (Vec3.length(Entities.getEntityProperties(_entityID, ["velocity"]).velocity) > VELOCITY_LIMITER_MIN) {
+            playSound();
+        }
+        Script.setTimeout(function () {
+            var velocity = Entities.getEntityProperties(_entityID, ["velocity"]).velocity;
+            if (Vec3.length(velocity) < VELOCITY_LIMITER_MAX && Vec3.length(velocity) > VELOCITY_LIMITER_MIN) {
+                Entities.editEntity(_entityID, {velocity: Vec3.multiply(VELOCITY_MULTIPLIER, velocity)});
+            }
+        }, BOUNCE_DELAY);
+    }
+});
